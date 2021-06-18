d434ccd95438516ca926076d4760e870a8b349ff
saradisimone <sara.disimone@monash.edu>
2021-06-18 11:31:55 +1000

    Add support for division operator

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ def compute(expression):
     elif operator == '-':
         return num0 - num1
      elif operator == '/':
-        return num0 - num1
+        return num0 / num1
     else:
         print('unknown operator!')
         return None
